f0b8957fc442147d7924203528915420b7a12185
Aung Myo Oo <aungmyooo@Aungs-MBP.lan>
2022-01-27 17:58:39 +0630

    added html  div formatter sample

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,43 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import ChordSheetJS from "chordsheetjs"; 
+import "react-jinke-music-player/assets/index.css";
 
 function App() {
   
 const getDisp = () => {
   const chordSheet = `
-  Am         C/G        F          C
-Let it be, let it be, let it be, let it be
-C                G              F  C/E Dm C
-Whisper words of wisdom, let it be`.substring(1);
 
-const parser = new ChordSheetJS.ChordSheetParser();
+{title: Let it be}
+{subtitle: ChordSheetJS example version}
+{Chorus}
+
+Let it [Am]be, let it [C/G]be, let it [F]be, let it [C]be
+[C]Whisper words of [G]wisdom, let it [F]be [C/E] [Dm] [C]`.substring(1);
+
+const parser = new ChordSheetJS.ChordProParser();
 const song = parser.parse(chordSheet);
 const formatter = new ChordSheetJS.HtmlTableFormatter();
 const disp = formatter.format(song);
+console.log(disp);
+return disp;
+}
+
+const getHtmlDivFormatter = () => {
+  const chordSheet = `
+
+{title: Let it be}
+{subtitle: ChordSheetJS example version}
+{Chorus}
+
+Let it [Am]be, let it [C/G]be, let it [F]be, let it [C]be
+[C]Whisper words of [G]wisdom, let it [F]be [C/E] [Dm] [C]`.substring(1);
+
+const parser = new ChordSheetJS.ChordProParser();
+const song = parser.parse(chordSheet);
+const formatter = new ChordSheetJS.HtmlDivFormatter();
+const disp = formatter.format(song);
+console.log(disp);
 return disp;
 }
 
@@ -24,12 +47,24 @@ return disp;
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Chord!</p>
+        <p>HtmlTableFormatter!</p>
+        
         <div className="p-2 m-2 h-4">
                         <pre
-                          className="p-2 w-96 textarea textarea-primary resize-none h-32 overflow-y-auto"
+                          className="p-2 w-96 textarea resize-none h-32 overflow-y-auto"
                           dangerouslySetInnerHTML={{
                             __html: getDisp(),
+                          }}
+                        />
+                      </div>
+
+                      <p>HtmlDivFormatter!</p>
+        
+        <div className="p-2 m-2 h-4">
+                        <pre
+                          className="p-2 w-96 textarea resize-none h-32 overflow-y-auto"
+                          dangerouslySetInnerHTML={{
+                            __html: getHtmlDivFormatter(),
                           }}
                         />
                       </div>
