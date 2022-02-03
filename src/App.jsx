d8e8f0c4d7021167877a11a5af0c67a1c2343482
Aung Myo Oo <aungmyooo@Aungs-MacBook-Pro.local>
2022-02-04 05:19:59 +0630

    \n

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,20 @@ function App() {
 const getDisp = () => {
   const chordSheet = `
 
-{title: Let it be}
-{subtitle: ChordSheetJS example version}
-{Chorus}
+Intro : Dbm A B A B Dbm A B Gb Dbm Dbm
+[Dbm]ငါ့နေ့ရက်များ . . . [A]အတ္တရဲ့ပြေးလမ်းပေါ် တစ်ယောက်ထဲ . . .[Dbm]တရွေ့ရွေ့နဲ့ဖြတ်သန်း . . .[A]နေဝင်ညများကူးဖြတ်လိုက်စမ်း . . .[Dbm]
+[Dbm]ရိုက်ခတ်များ . . .[A]ပစ်လွှတ်တဲ့မြားသွားတွေလိုပဲ[Dbm]. . . [Dbm]ရင်မှာစူးနစ်လို့နေခဲ့ . . . အဆိပ်တတ်ခဲ့ပြီ[A]မင်းတွေ့လား . . .[Dbm]
+*[B]အတ္တရဲ့ရိုက်ချက်များ/ပြေးလမ်းက . . . [A](ငါ)လွယ်လွယ်မေ့ထားမလား/ထွက်သွားမလား . . .
+[B]သွေးအေးစွာ အရှုံးပေးမလား . . .[Ab]နောက်လှည့်ပြန်မလား . . .
+[Dbm]ဒဏ်ရာများ . . .[A]မေ့ပစ်နိုင်လား . . . [E][B][AB](အနိုင်ယူချင်တဲ့ငါစိတ်များ) . . .[A][E][Dbm][B][B] [A][B]
 
-Let it [Am]be, let it [C/G]be, let it [F]be, let it [C]be
-[C]Whisper words of [G]wisdom, let it [F]be [C/E] [Dm] [C]`.substring(1);
+[Dbm]အတ္တညများ . . . [A]တစ်ခုခုတော့ မှားယွင်းနေမှာပဲ . . .
+[Dbm]သွေးပျက်ရင်းဖြတ်သန်း . . .[A]နောက်ကအရိပ်တွေ လိုက်လို့လာခဲ့. . .[Dbm]
+[Dbm]ငါစိတ်ဓါတ်များ . . .[A]တမြည့်မြည့်နဲ့ကျွမ်းတောက်လောင်နေခဲ့[Dbm
+[Dbm]ငါဟာပြေးရင်းပြေးနေခဲ့ . . . တစ်နေ့နေ့ရောက်မယ်[A]မင်းကြည့်နေလိုက် . . .
+[Dbm]တစ်နေ့နေ့တော့ရောက်မှာပဲ . . . 
+Music & (Cho with Key F) FADE
+`.substring(1);
 
 const parser = new ChordSheetJS.ChordProParser();
 const song = parser.parse(chordSheet);
@@ -59,14 +67,14 @@ return disp;
 
                       <p>HtmlDivFormatter!</p>
         
-        <div className="p-2 m-2 h-4">
+        {/* <div className="p-2 m-2 h-4">
                         <pre
                           className="p-2 w-full textarea resize-none h-32 overflow-y-auto"
                           dangerouslySetInnerHTML={{
                             __html: getHtmlDivFormatter(),
                           }}
                         />
-                      </div>
+                      </div> */}
         
         
       </header>
